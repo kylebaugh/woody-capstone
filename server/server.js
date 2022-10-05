60ca3ae716fd53081b9c4b2540ca1d32b0c7fc4a
Kyle Baugh <kylebaugh1@gmail.com>
2022-10-05 11:51:52 -0600

    asdf

--- server/server.js
+++ server/server.js
@@ -33,5 +33,5 @@ app.post("/api/locations", createLocation);
 app.put("/api/locations/:id", updateLocationTime);
 
 
-const port = process.PORT || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Checking in at port ${port}`));
